c3beaf42ecc732a97ba1c88430f3e71a9fcd12eb
Yasin <yasincaykur@gmail.com>
2022-09-04 22:35:41 +0300

    logger middleware added

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::middleware::Logger;
+use env_logger::Env;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -23,12 +25,19 @@ async fn manual_hello() -> impl Responder {
 async fn main() -> std::io::Result<()> {
     let port = 8080;
     println!("running on {}", port);
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
     HttpServer::new( || {
         App::new()
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
+            // hmm prefix
+            .service(
+                web::scope("/test")
+                .route("/hey", web::get().to(manual_hello))
+            )
             .service(hello)
             .service(echo)
             .service(health_check)
-            .route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", port))?
     .run()
